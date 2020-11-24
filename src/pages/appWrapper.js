9ee198515c876e3bb747ee8f6898897be2c34b80
mani <>
2020-11-24 23:03:48 +0530

    added changes

--- src/pages/appWrapper.js
+++ src/pages/appWrapper.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import ComponentA from './Components/ComponentA'
+import ComponentA from './Components/ComponentA';
 import ComponentB from './Components/ComponentB';
 import ShowActiveComponent from './Components/ShowActiveComponent';
-import { withKeyboardShortcuts} from '../package/react-keypress'
+import { withKeyboardShortcuts } from '../package/react-keypress';
 
 class AppWrapper extends Component {
   render() {
     const { activeShortCuts } = this.props;
     return (
-      <div className='ContainerClass'>
-          <div className='leftSide'>
-             <ComponentA></ComponentA>
-             <ComponentB/>
-          </div>
-          <div className='rightSide'>
-            <ShowActiveComponent activeKeys={activeShortCuts}/>
+      <div className="ContainerClass">
+        <div className="leftSide">
+          <ComponentA />
+          <ComponentB />
+        </div>
+        <div className="rightSide">
+          <ShowActiveComponent activeKeys={activeShortCuts} />
+          <p
+            style={{
+              float: 'right',
+              padding: 0,
+              margin: '50px'
+            }}
+          >
+            What to know more about me (React keyboard shortcut).{' '}
+            <a
+              target="_blank"
+              href="https://github.com/manishankardurai/keyboardShortCuts/blob/master/README.md"
+            >
+              {' '}
+              Have a look at here
+            </a>
+          </p>
         </div>
       </div>
-    )
+    );
   }
 }
 
